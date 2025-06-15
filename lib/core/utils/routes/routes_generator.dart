9df4a40709c5194895dad0ff956652ca0fd3d108
FatmaNasser29 <fatma.nasser1213@gmail.com>
2025-06-16 02:07:07 +0300

    create splash and on boarding screen

--- lib/core/utils/routes/routes_generator.dart
+++ lib/core/utils/routes/routes_generator.dart
@@ -1,15 +1,34 @@
 import 'package:fitness_app/core/utils/routes/routes_name.dart';
-import 'package:fitness_app/onboarding.dart';
+import 'package:fitness_app/features/onBoarding/on_boarding_one.dart';
+import 'package:fitness_app/features/onBoarding/on_boarding_three.dart';
+import 'package:fitness_app/features/onBoarding/on_boarding_two.dart';
+import 'package:fitness_app/features/splash/splash_view.dart';
 import 'package:flutter/material.dart';
 
 class RoutesGenerator {
   static Route<dynamic>? onGenerator(RouteSettings settings) {
     switch (settings.name) {
-      case RoutesName.onBoarding:
+      case RoutesName.onBoardingOne:
         return MaterialPageRoute(
-          builder: (context) => const Onboarding(),
+          builder: (context) => const OnBoardingOne(),
           settings: settings,
         );
+      case RoutesName.onBoardingTwo:
+        return MaterialPageRoute(
+          builder: (context) => const OnBoardingTwo(),
+          settings: settings,
+        );
+      case RoutesName.onBoardingThree:
+        return MaterialPageRoute(
+          builder: (context) => const OnBoardingThree(),
+          settings: settings,
+        );
+      case RoutesName.splash:
+        return MaterialPageRoute(
+          builder: (context) => const SplashView(),
+          settings: settings,
+        );
+
       default:
         return null;
     }
---
+++ lib/features/onBoarding/on_boarding_three.dart
@@ -0,0 +1,24 @@
+import 'package:fitness_app/core/utils/routes/routes_name.dart';
+import 'package:fitness_app/core/utils/theme/app_assets.dart';
+import 'package:fitness_app/features/onBoarding/on_boarding_widget.dart';
+import 'package:flutter/material.dart';
+
+class OnBoardingThree extends StatelessWidget {
+  const OnBoardingThree({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return OnBoardingWidget(
+      image: AppAssets.onBoardingThree,
+      title: "NO MORE EXCUSES \n Do It Now",
+      contain:
+          "Lorem ipsum dolor sit amet consectetur. Eu urna ut gravida quis id pretium purus. Mauris massa ",
+      skip: false,
+      onPressedElevated: () {},
+      activeDotState: 2,
+      onPressedOutLined: () {
+        Navigator.pushNamed(context, RoutesName.onBoardingTwo);
+      },
+    );
+  }
+}
