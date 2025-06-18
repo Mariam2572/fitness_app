53cc963feaacfc8800a517cdcf5e620f98be7d4c
FatmaNasser29 <fatma.nasser1213@gmail.com>
2025-06-18 20:03:58 +0300

    refactor code

--- lib/core/utils/routes/routes_generator.dart
+++ lib/core/utils/routes/routes_generator.dart
@@ -3,10 +3,10 @@ import 'package:fitness_app/core/utils/routes/routes_name.dart';
 import 'package:fitness_app/feature/login/domain/usecases/login_usecase.dart';
 import 'package:fitness_app/feature/login/presentation/view/login_screen.dart';
 import 'package:fitness_app/feature/login/presentation/view_model/login_cubit.dart';
-import 'package:fitness_app/features/onBoarding/on_boarding_one.dart';
-import 'package:fitness_app/features/onBoarding/on_boarding_three.dart';
-import 'package:fitness_app/features/onBoarding/on_boarding_two.dart';
-import 'package:fitness_app/features/splash/splash_view.dart';
+import 'package:fitness_app/feature/onBoarding/on_boarding_one.dart';
+import 'package:fitness_app/feature/onBoarding/on_boarding_three.dart';
+import 'package:fitness_app/feature/onBoarding/on_boarding_two.dart';
+import 'package:fitness_app/feature/splash/splash_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
--- lib/feature/onBoarding/on_boarding_three.dart
+++ lib/feature/onBoarding/on_boarding_three.dart
@@ -1,6 +1,6 @@
 import 'package:fitness_app/core/utils/routes/routes_name.dart';
 import 'package:fitness_app/core/utils/theme/app_assets.dart';
-import 'package:fitness_app/features/onBoarding/on_boarding_widget.dart';
+import 'package:fitness_app/feature/onBoarding/on_boarding_widget.dart';
 import 'package:flutter/material.dart';
 
 class OnBoardingThree extends StatelessWidget {
@@ -14,7 +14,9 @@ class OnBoardingThree extends StatelessWidget {
       contain:
           "Lorem ipsum dolor sit amet consectetur. Eu urna ut gravida quis id pretium purus. Mauris massa ",
       skip: false,
-      onPressedElevated: () {},
+      onPressedElevated: () {
+        Navigator.pushNamed(context, RoutesName.login);
+      },
       activeDotState: 2,
       onPressedOutLined: () {
         Navigator.pushNamed(context, RoutesName.onBoardingTwo);
