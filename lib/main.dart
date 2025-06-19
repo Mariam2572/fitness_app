9bd516904c90bc084f0877001a7db8f8cb5c5c39
Mariam2572 <mariamkamel791@gmail.com>
2025-06-19 14:32:58 +0300

    refactor: update localization imports and remove unused localization files

--- lib/main.dart
+++ lib/main.dart
@@ -6,10 +6,9 @@ import 'package:fitness_app/core/utils/simple_bloc_observer.dart';
 import 'package:fitness_app/core/utils/theme/app_theme.dart';
 import 'package:fitness_app/features/auth/register/domain/use_cases/register_use_case.dart';
 import 'package:fitness_app/features/auth/register/presentation/view_model/cubit/register_cubit.dart';
-import 'package:fitness_app/l10n/app_localizations.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:provider/provider.dart';
 
 Future<void> main() async {
