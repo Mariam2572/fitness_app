d4b47716422768d6ec9fa0111f2709b52d30a9fc
Mariam2572 <mariamkamel791@gmail.com>
2025-06-21 17:17:31 +0300

    feat: implement ExerciseRemoteDataSource and ExerciseRepo interfaces

---
+++ lib/features/exercise/data/data_source/exercise_remote_data_source_impl.dart
@@ -0,0 +1,3 @@
+import 'package:fitness_app/features/exercise/domain/data_source/exercise_remote_data_source.dart';
+
+class ExerciseRemoteDataSourceImpl  implements ExerciseRemoteDataSource{}
---
+++ lib/features/exercise/data/repo_impl/exercise_repo_impl.dart
@@ -0,0 +1,3 @@
+import 'package:fitness_app/features/exercise/domain/repos/exercise_repo.dart';
+
+class ExerciseRepoImpl implements ExerciseRepo{}
