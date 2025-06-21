959c648a042ad08a297d1601dbfe28eeb45964c8
Samah <Samahosama112@icloud.com>
2025-06-21 13:49:51 +0300

    feat: add localization support for Arabic and English

--- lib/core/utils/helper/extention.dart
+++ lib/core/utils/helper/extention.dart
@@ -1,6 +1,7 @@
 
+import 'package:fitness_app/l10n/app_localizations.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
 
 extension LocalizationHelper on BuildContext {
   AppLocalizations get loc => AppLocalizations.of(this)!;
