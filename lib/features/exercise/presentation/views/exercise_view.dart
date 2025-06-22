64edb94db268a921ff8778e474fccbc586595577
Samah <Samahosama112@icloud.com>
2025-06-22 21:42:45 +0300

    feat: implement ExerciseView and ExerciseWidgetItem with layout and styling

--- lib/features/exercise/presentation/views/exercise_view.dart
+++ lib/features/exercise/presentation/views/exercise_view.dart
@@ -1,3 +1,8 @@
+import 'package:fitness_app/core/utils/helper/extention.dart';
+import 'package:fitness_app/core/utils/theme/app_assets.dart';
+import 'package:fitness_app/core/utils/theme/app_colors.dart';
+import 'package:fitness_app/core/utils/widgets/custom_glass_container.dart';
+import 'package:fitness_app/features/exercise/presentation/views/widgets/exercise_widget_item.dart';
 import 'package:flutter/material.dart';
 
 class ExerciseView extends StatelessWidget {
@@ -5,6 +10,35 @@ class ExerciseView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Stack(
+        children: [
+          Image.asset(
+            AppAssets.homeBackground,
+            fit: BoxFit.cover,
+            height: double.infinity,
+            width: double.infinity,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16),
+            child: CustomGlassContainer(
+              child: ListView.separated(
+                itemBuilder: (BuildContext context, int index) {
+                  return const ExerciseWidgetItem();
+                },
+                separatorBuilder: (BuildContext context, int index) {
+                  return const Divider(
+                    color: AppColors.neutral50,
+                    thickness: .5,
+                  );
+                },
+                itemCount: 5,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
+
---
+++ lib/features/exercise/presentation/views/widgets/exercise_widget_item.dart
@@ -0,0 +1,62 @@
+import 'package:fitness_app/core/utils/helper/extention.dart';
+import 'package:fitness_app/core/utils/theme/app_assets.dart';
+import 'package:fitness_app/core/utils/theme/app_colors.dart';
+import 'package:flutter/material.dart';
+class ExerciseWidgetItem extends StatelessWidget {
+  const ExerciseWidgetItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+
+        children: [
+          ClipRRect(
+            borderRadius: const BorderRadius.only(
+              topLeft: Radius.circular(12),
+              bottomLeft: Radius.circular(12),
+            ),
+            child: Image.asset(AppAssets.exerciseImage, fit: BoxFit.fill),
+          ),
+          const SizedBox(width: 16),
+
+          Expanded(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Bench Press',
+                  style: context.textTheme.titleSmall?.copyWith(
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                const SizedBox(height: 4),
+                Text(
+                  '3 Groups * 15 Times Lorem Ipsum Dolor Sit Amet Consectetur. Tempus',
+                  softWrap: true,
+                  style: context.textTheme.bodyMedium,
+                ),
+              ],
+            ),
+          ),
+
+          GestureDetector(
+            onTap: () {},
+            child: const CircleAvatar(
+              backgroundColor: AppColors.mainRed,
+              radius: 15,
+              child: Icon(
+                Icons.play_arrow,
+                color: AppColors.neutral90,
+                size: 30,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
