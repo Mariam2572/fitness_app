0f741e41c62610d6af5e22cc2c743cb49372fa82
Samah <Samahosama112@icloud.com>
2025-06-23 12:06:57 +0300

    feat: implement remote data source and use case to fetch exercises by prime mover and difficulty level

--- lib/features/exercise/domain/data_source/exercise_remote_data_source.dart
+++ lib/features/exercise/domain/data_source/exercise_remote_data_source.dart
@@ -1,7 +1,11 @@
 import 'package:fitness_app/core/base/api_result.dart';
+import 'package:fitness_app/features/exercise/data/models/exercise_by_prime_mover_and_difficulty_model/exercise_by_prime_mover_and_difficulty_response.dart';
 import 'package:fitness_app/features/exercise/data/models/levels_by_muscles_model.dart';
 
-abstract interface class ExerciseRemoteDataSource{
-
-  Future<ApiResult<LevelsByMusclesModel>> getLevelsByMuscles({required String primeMoverMuscleId});
+abstract interface class ExerciseRemoteDataSource {
+  Future<ApiResult<LevelsByMusclesModel>> getLevelsByMuscles({
+    required String primeMoverMuscleId,
+  });
+  Future<ApiResult<ExerciseByPrimeMoverAndDifficultyResponse>>
+  getExerciseByMoverAndDifficultyLevel({required String primeMoverMuscleId , required String difficultyLevelId});
 }
