1eb3835f7d646153c35eec0f85436563160e7b9d
Jimmybasha <gemiahmed2002@gmail.com>
2025-06-23 21:44:15 +0300

    Added Unit Testing -Ui Testing is remaining

--- test/test/unit_test_layout_test.dart
+++ test/test/unit_test_layout_test.dart
@@ -1,27 +1,28 @@
-// import 'package:fitness_app/features/layOut/lay_out.dart';
-// import 'package:fitness_app/features/profile/profile_view.dart';
-// import 'package:fitness_app/features/smartCoach/smart_coach_view.dart';
-// import 'package:fitness_app/features/workOuts/work_outs_view.dart';
-// import 'package:flutter/material.dart';
-// import 'package:flutter_test/flutter_test.dart';
-//
-//
-// void main() {
-//   testWidgets('LayOut navigation works correctly', (WidgetTester tester) async {
-//     await tester.pumpWidget(const MaterialApp(home: LayOut()));
-//
-//     expect(find.byType(HomeView), findsOneWidget);
-//
-//     await tester.tap(find.text("Smart Chat"));
-//     await tester.pumpAndSettle();
-//     expect(find.byType(SmartCoachView), findsOneWidget);
-//
-//     await tester.tap(find.text("Work Outs"));
-//     await tester.pumpAndSettle();
-//     expect(find.byType(WorkOutsView), findsOneWidget);
-//
-//     await tester.tap(find.text("Profile"));
-//     await tester.pumpAndSettle();
-//     expect(find.byType(ProfileView), findsOneWidget);
-//   });
-// }
+import 'package:fitness_app/features/home/home/presentation/views/home_view.dart';
+import 'package:fitness_app/features/layOut/lay_out.dart';
+import 'package:fitness_app/features/profile/profile_view.dart';
+import 'package:fitness_app/features/smartCoach/smart_coach_view.dart';
+import 'package:fitness_app/features/workOuts/work_outs_view.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+
+void main() {
+  testWidgets('LayOut navigation works correctly', (WidgetTester tester) async {
+    await tester.pumpWidget(const MaterialApp(home: LayOut()));
+
+    expect(find.byType(HomeView), findsOneWidget);
+
+    await tester.tap(find.text("Smart Chat"));
+    await tester.pumpAndSettle();
+    expect(find.byType(SmartCoachView), findsOneWidget);
+
+    await tester.tap(find.text("Work Outs"));
+    await tester.pumpAndSettle();
+    expect(find.byType(WorkOutsView), findsOneWidget);
+
+    await tester.tap(find.text("Profile"));
+    await tester.pumpAndSettle();
+    expect(find.byType(ProfileView), findsOneWidget);
+  });
+}
